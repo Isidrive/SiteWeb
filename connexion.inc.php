04950b7924619ffd85ba2049a2a91f071420f58d
isidrive <isidrive.professionnel@gmail.com>
2022-09-01 09:42:43 +0200

    update bdd

--- connexion.inc.php
+++ connexion.inc.php
@@ -1,3 +1,4 @@
 <?php
-$cnn = new PDO("odbc:Driver={SQL Server};Server=WINDOWSSIO7\SQLSERVER2012;Database=UDrive_perrodon_goncalves;Uid=sa;Pwd=mdpsa;");
+$cnn = new PDO("odbc:Driver={SQL Server};Server=SRV-APPLICATION;Database=Isidrive;Uid=alexandre;Pwd=A*14nJqXS+;");
 ?>
+
