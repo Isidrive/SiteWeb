9c34b62d6ad90cda94bbe94c49d3d076e0f702d6
isidrive <isidrive.professionnel@gmail.com>
2022-09-01 10:10:37 +0200

    update bdd

--- connexion.inc.php
+++ connexion.inc.php
@@ -2,8 +2,8 @@
 $cnn = new PDO("odbc:Driver={SQL Server};Server=SRV-APPLICATION\SQLSERVER2019;Database=Isidrive;Uid=ISIDRIVE\alexandre;Pwd=A*14nJqXS+");
 
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($cnn->connect_error) {
+  die("Connection failed: " . $cnn->connect_error);
 }
 echo "Connected successfully";
 ?>
