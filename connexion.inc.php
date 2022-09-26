f7eb8172c2e2dc8217ddb4c08146c627127ccf94
doccreeps <dorian1998dino@hotmail.com>
2022-09-26 10:21:01 +0200

    update: debug bdd

--- connexion.inc.php
+++ connexion.inc.php
@@ -1,8 +1,18 @@
 <?php
-$host ="eu-cdbr-west-03.cleardb.net";
-$dbname = "heroku_ed5518a9c03f82b";
-$user = "b6b5a59b76adf9";
-$pass ="1f4603cc";
-$cnn = new PDO("mysql:host=$host; dbname=$dbname;", $user, $pass);
+
+try{
+    $host ="eu-cdbr-west-03.cleardb.net";
+    $dbname = "heroku_ed5518a9c03f82b";
+    $user = "b6b5a59b76adf9";
+    $pass ="1f4603cc";
+    $cnn = new PDO("mysql:host=$host; dbname=$dbname;", $user, $pass);
+    die(json_encode(array('outcome' => true)));
+}catch(PDOException $ex){
+    die(json_encode(array(
+        'outcome' => false,
+        'message' => 'Unable to connect'
+    )));
+}
+
 ?>
 
