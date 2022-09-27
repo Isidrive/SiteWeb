5eef5fe892f5721af54ea049ed8aa956ee648019
doccreeps <dorian1998dino@hotmail.com>
2022-09-27 17:17:55 +0200

    upgrade: connexion

--- produits.php
+++ produits.php
@@ -115,7 +115,15 @@ if (isset($_GET['id']) == true) {
 		$uneligne = $reqresult->fetch();
 		$n=1;
 		?>
-
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
         <nav aria-label="Page">
   <ul class="pagination">
 
