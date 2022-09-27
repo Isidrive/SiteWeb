4121e652f9455cf015924d9709457710b0a999e2
doccreeps <dorian1998dino@hotmail.com>
2022-09-27 13:55:46 +0200

    upgrade: inscription*

--- inscriptioncompte.php
+++ inscriptioncompte.php
@@ -21,7 +21,7 @@ include("includes/pageentete.php");
 					</div>
 					<div class="form-group col-md-4">
 						<label for="inputCity">Téléphone</label>
-						<input type="text" class="form-control" id="inputCity" name="txttel">
+						<input type="text" class="form-control" id="inputCity" name="txttel" pattern="[0-9]">
 					</div>
 					<div class="form-group col-md-6">
 						<label for="inputEmail4">Email</label>
